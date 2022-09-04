4bb72ea3ce0dab21181ba059dee8d6ecd0d5c4cc
ti-metiss <coutenay.yoan@hotmail.fr>
2022-09-04 12:14:15 +0200

    fing + pagination

--- src/Controller/PersonneController.php
+++ src/Controller/PersonneController.php
@@ -8,9 +8,55 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/personne')]
 class PersonneController extends AbstractController
 {
-    #[Route('/personne/add', name: 'add_personne')]
+    #[Route('/', name: 'list_personne')]
+    public function index(ManagerRegistry $doctrine): Response
+    {
+        $repository = $doctrine->getRepository(Personne::class);
+        $personnes = $repository->findAll();
+        return $this->render('personne/index.html.twig', [
+            'personnes' => $personnes,
+            'isPaginated' => true,
+        ]);
+    }
+
+    #[Route('/all/{page?1}/{nbr?12}', name: 'all_personne')]
+    public function indexAll(ManagerRegistry $doctrine, $page, $nbr): Response
+    {
+        $repository = $doctrine->getRepository(Personne::class);
+        $nbPersonne = $repository->count([]);
+        $nbPage = ceil($nbPersonne / $nbr);
+        $personnes = $repository->findBy([], [], $nbr, ($page - 1) * $nbr);
+        return $this->render('personne/index.html.twig', [
+            'personnes' => $personnes,
+            'isPaginated' => true,
+            'nbPage' => $nbPage,
+            'page' => $page,
+            'nbr' => $nbr
+        ]);
+    }
+
+    // avec param converteur, on convertir le parametres en objet
+    #[Route('/{id<\d+>}', name: 'detail_personne')]
+    public function detail(/*ManagerRegistry $doctrine, $id*/Personne $personne = null): Response
+    {
+        /*
+        $repository = $doctrine->getRepository(Personne::class);
+        $personne = $repository->find($id);
+        */
+        if (!$personne) {
+            // $this->addFlash('error', "La personne d'id $id n'existe pas");
+            $this->addFlash('error', "La personne n'existe pas");
+            return $this->redirectToRoute('list_personne');
+        }
+        return $this->render('personne/detail.html.twig', [
+            'personne' => $personne
+        ]);
+    }
+
+    #[Route('/add', name: 'add_personne')]
     public function addPersonne(ManagerRegistry $doctrine): Response
     {
         $entityManager = $doctrine->getManager();
